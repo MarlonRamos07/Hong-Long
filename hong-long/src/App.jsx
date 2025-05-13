05755ee866f082d69949668a3a0fea6bd7a9c3ac
Marlon Ramos <marlonrdacosta@gmail.com>
2025-05-13 15:47:07 -0300

    Componente Header

---
+++ hong-long/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Componentes/Header'
+
+function App() {
+  
+
+  return (
+    <>
+       <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ hong-long/src/Componentes/Header.jsx
@@ -0,0 +1,25 @@
+import WhiteLogo from '../assets/White_Logo.svg'
+import BurguerMenu from '../assets/burguer__menu.svg'
+
+
+function Header(){
+    return(
+      
+        <header className=' flex items-center justify-between md:px-48  px-9'>
+            <img className='h-[8.625rem] md:h-[15.75rem]'src={WhiteLogo} alt="Logo Branco"/>
+            <nav>
+                <ul className='hidden md:flex gap-10 font-["Sawarabi Gothic"] text-white text-[1.438rem]' >
+                  <li className='hover:text-black'><a href="">Home</a></li>
+                  <li className='hover:text-black'><a href="">Pratos</a></li>
+                  <li className='hover:text-black'><a href="">Sobre nós</a></li>
+                  <li className='hover:text-black'><a href="">Contato</a></li>
+                </ul>                
+            </nav>
+            <button className='md:hidden'>
+                <img className='md:hidden' src={BurguerMenu} alt=""/>
+            </button>
+        </header>
+    )
+}
+
+export default Header
