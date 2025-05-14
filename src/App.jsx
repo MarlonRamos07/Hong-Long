1caf53114ede6cde50e6a6bf96e2de41bf5a2ced
Marlon Ramos <marlonrdacosta@gmail.com>
2025-05-14 18:29:59 -0300

    Ajustando sessão de testemunhos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import Testimonials from './Componentes/Testimonials'
-// import About from './Componentes/About'
-// import Header from './Componentes/Header'
-// import Hero from './Componentes/Hero'
-// import Menu from './Componentes/Menu'
+import About from './Componentes/About'
+import Header from './Componentes/Header'
+import Hero from './Componentes/Hero'
+import Menu from './Componentes/Menu'
 
 function App() {
   
 
   return (
     <>
+    <Header/>
+    <Hero/>
+    <About/>
+    <Menu/>
     <Testimonials/>
     </>
   )
--- src/Componentes/Testimonials.jsx
+++ src/Componentes/Testimonials.jsx
@@ -6,14 +6,18 @@ import CardPessoa from './CardPessoa'
 
 function Testimonials () {
     return(
-        <section className='flex flex-col gap-6 mt-10'>
-            <h2 className='font-[Montserrat] text-white w-[22.063rem] h-[3.625rem] text-2xl font-bold text-center mx-auto'>Descubra o que nossos clientes dizem</h2>
-            <h4 className='font-[Sawarabi Gothic] text-center text-[1rem] w-[21.25rem] mx-auto h=[3rem] text-white '>Descubra o que torna o Hong Long o favorito de quem ama sabor e tradição!</h4>
-            <div className='md:flex md:flex-row md:mx-auto md:justify-center  flex-col gap-8'>
-                <img className='hidden md:block'src={leftArrow}/>
+        <section className='flex flex-col gap-10 mt-40'>
+            <h2 className='md:w-[58.125rem] md:h-[3.438rem] md:text-[2.813rem]    font-[Montserrat] text-white w-[22.063rem] h-[3.625rem] text-2xl font-bold text-center mx-auto'>Descubra o que nossos clientes dizem</h2>
+            <h4 className='md:w-[58.125rem] md:h-[2.375rem] md:text-[1.625rem] font-[Sawarabi Gothic] text-center text-[1rem] w-[21.25rem] mx-auto h=[3rem] text-white '>Descubra o que torna o Hong Long o favorito de quem ama sabor e tradição!</h4>
+            <div className='hidden md:flex md:flex-row md:mx-auto md:justify-center md:mt-28 gap-8 flex-col'>
+                <img className='hidden md:block cursor-pointer'src={leftArrow}/>
                 <CardPessoa image={CardMan} title={'Antonio Nunes'} description={'“O melhor jantar asiático que já tive! Atendimento impecável, pratos saborosos e tudo servido com muito capricho. O ambiente também é lindo, super aconchegante!”'}/>
                 <CardPessoa image={CardWomen} title={'Daenerys Silva'} description={'“Fui ao Hong Long com minha família e cada detalhe nos encantou — do aroma ao atendimento. O Yakissoba estava incrível e o ambiente, acolhedor. Voltaremos com certeza!”'}/>
-                <img className='hidden md:block' src={rightArrow}/>
+                <img className='hidden md:block cursor-pointer' src={rightArrow}/>
+            </div>
+            <div className='flex flex-col gap-8 md:hidden'>
+            <CardPessoa image={CardMan} title='Antonio Nunes' description='“O melhor jantar asiático que já tive! Atendimento impecável, pratos saborosos e tudo servido com muito capricho. O ambiente também é lindo, super aconchegante!”' />
+            <CardPessoa image={CardWomen} title='Daenerys Silva' description='“Fui ao Hong Long com minha família e cada detalhe nos encantou — do aroma ao atendimento. O Yakissoba estava incrível e o ambiente, acolhedor. Voltaremos com certeza!”' />
             </div>
         </section>
     )
