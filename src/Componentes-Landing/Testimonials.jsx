428bf41e9b2a5eb1a88f98e6c8f0b38fc36724d7
Marlon Ramos <marlonrdacosta@gmail.com>
2025-05-26 20:56:13 -0300

    Feature Carrossel

--- src/Componentes-Landing/Testimonials.jsx
+++ src/Componentes-Landing/Testimonials.jsx
@@ -1,24 +1,11 @@
-import leftArrow from '../assets/left_arrow.svg'
-import rightArrow from '../assets/Right_Arrow.svg'
-import CardWomen from '../assets/Card_Women.svg'
-import CardMan from '../assets/Card_Man.svg'
-import CardPessoa from './CardPessoa'
+import Carrossel from "./Carrossel"
 
 function Testimonials () {
     return(
         <section className='flex flex-col gap-10 mt-40'>
             <h2 className='md:w-[58.125rem] md:h-[3.438rem] md:text-[2.813rem]    font-[Montserrat] text-white w-[22.063rem] h-[3.625rem] text-2xl font-bold text-center mx-auto'>Descubra o que nossos clientes dizem</h2>
             <h4 className='md:w-[58.125rem] md:h-[2.375rem] md:text-[1.625rem] font-[Sawarabi Gothic] text-center text-[1rem] w-[21.25rem] mx-auto h=[3rem] text-white '>Descubra o que torna o Hong Long o favorito de quem ama sabor e tradição!</h4>
-            <div className='hidden md:flex md:flex-row md:mx-auto md:justify-center md:mt-28 gap-8 flex-col'>
-                <img className='hidden md:block cursor-pointer'src={leftArrow}/>
-                <CardPessoa image={CardMan} title={'Antonio Nunes'} description={'“O melhor jantar asiático que já tive! Atendimento impecável, pratos saborosos e tudo servido com muito capricho.”'}/>
-                <CardPessoa image={CardWomen} title={'Daenerys Silva'} description={'“Fui ao Hong Long com minha família e cada detalhe nos encantou — do aroma ao atendimento.”'}/>
-                <img className='hidden md:block cursor-pointer' src={rightArrow}/>
-            </div>
-            <div className='flex flex-col gap-8 md:hidden'>
-            <CardPessoa image={CardMan} title='Antonio Nunes' description='“O melhor jantar asiático que já tive! Atendimento impecável, pratos saborosos e tudo servido com muito capricho.”' />
-            <CardPessoa image={CardWomen} title='Daenerys Silva' description='“Fui ao Hong Long com minha família e cada detalhe nos encantou — do aroma ao atendimento.”' />
-            </div>
+           <Carrossel/>
         </section>
     )
 }
